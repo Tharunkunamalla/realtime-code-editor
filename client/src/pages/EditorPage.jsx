eed9fb2325cc6c717e2f6b4f76633da2c1716617
Tharunkunamalla <kunamallatharun701@gmail.com>
2026-02-14 22:45:19 +0530

    ui

--- client/src/pages/EditorPage.jsx
+++ client/src/pages/EditorPage.jsx
@@ -179,7 +179,7 @@ const EditorPage = () => {
                     <div className="logo">
                         <img
                             className="logoImage"
-                            src="/code-sync.png"
+                            src="/logo.png"
                             alt="logo"
                         />
                     </div>
